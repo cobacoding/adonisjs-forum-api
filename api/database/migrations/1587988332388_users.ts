5d348aeb02657970d75a395366334d2edc5e2517
suka_coding <codingsuka@gmail.com>
2021-06-02 15:02:27 +0700

    Creating migrations

--- api/database/migrations/1587988332388_users.ts
+++ api/database/migrations/1587988332388_users.ts
@@ -9,7 +9,7 @@ export default class UsersSchema extends BaseSchema {
       table.string('email', 255).notNullable()
       table.string('password', 180).notNullable()
       table.string('remember_me_token').nullable()
-      table.timestamps(true)
+      table.timestamps()
     })
   }
 
